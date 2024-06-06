75f13f3783e6f4c956a2b2a03d69979f95deeef3
mohitsh7979 <mohitsh7979@gmail.com>
2024-06-06 18:06:18 +0530

    Update

---
+++ js/my.js
@@ -0,0 +1,50 @@
+// do-while 
+
+// let i = 3;
+
+// do{
+ 
+//     console.log("Hello Java Script")
+
+// }while (i==5)
+
+// let i = 0;
+
+// do{
+     
+//     console.log("Hello Java Script")
+//     i++;
+        
+// }while (i<=5)
+
+
+// while loops 
+
+// let i = 1;
+// let table = 5;
+// while (i<=10){
+
+//     console.log(table,"*",i,"=",table*i)
+//     console.log(`${table} * ${i} = ${table*i}`)
+//     i++
+// }
+
+// let age = 29;
+
+// console.log(`My age is ${age}`)
+
+
+// for loops 
+
+// for(let i = 0; i<10; i++){
+
+//     console.log(i)
+// }
+
+// let no = Math.floor(Math.random()*100)
+// console.log(no)
+
+
+
+
+
---
+++ js/readme.md
@@ -0,0 +1,7 @@
+# keywords let,var,const 
+# data type 
+# primitive--> number,string,boolean,undefined,null,symbol (call by value)
+# Not primitive --> objects , function (call by refrence)
+# conditional statements (if,else,else if,nested if )
+# loops (do-while,for,while)
+
