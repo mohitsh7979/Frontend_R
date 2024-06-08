3aad8ffbac1901adfec5bca28fb3fa4a71ff5135
mohitsh7979 <mohitsh7979@gmail.com>
2024-06-08 18:22:32 +0530

    Update

--- js/my.js
+++ js/my.js
@@ -44,6 +44,55 @@
 // let no = Math.floor(Math.random()*100)
 // console.log(no)
 
+// let i = 0;
+
+// while(i<=10){
+
+//     console.log(i)
+//     i++;
+// }
+
+// for(let i = 0 ; i<10 ; i++){
+//     console.log(i)
+// }
+
+// write a java script program to print all even numbers
+// Write a java script program to print sum of digits
+// Write a program to check given no is palindrom no or not
+// write a java script progarmm to check given no is armstrong number or not
+
+// a  = 50
+
+// for(let i = 1 ; i<a ; i++){
+
+//     if(i%2==0){
+//         console.log(i)
+//     }
+// }
+
+
+
+// let a = 54328
+// let sum = 0
+// while(a!=0){
+
+//     let rem = a%10
+//     sum = sum + rem
+//     a = parseInt(a/10)
+// }
+// console.log(sum)
+
+
+
+// let a = 12345;
+// let rev = 0;
+// while(a!=0){
+
+//     let rem = a%10;
+//     rev = rev * 10 + rem;
+//     a = parseInt(a/10);
+// }
+// console.log(rev);
 
 
 
---
+++ js/string.js
@@ -0,0 +1,18 @@
+a = "mohit sharma"
+// console.log(a)
+// console.log(typeof a)
+// console.log(a[0])
+// console.log(a[1])
+// console.log(a[2])
+// console.log(a[3])
+// console.log(a.length)
+
+// for(let i = 0 ; i<a.length; i=i+2){
+//     console.log(a[i])
+// }
+
+// for(let i of a){
+//     console.log(i)
+// }
+
+console.log(a.uppercase())
